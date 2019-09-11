407ff218d06588fa79632a891cead18887d544ea
Luis Fernando Manga <luis.manga@5by5.com.br>
2019-09-11 16:38:27 -0300

    estruturação do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,33 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  /* main */
+  {
+    path: '',
+    loadChildren: () => import('./pages/main/main.module').then(m => m.MainPageModule),
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+      },
+    ]
+  },
+  /* login */
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
+
+  /* signup */
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupPageModule)
+  },
+  /* not found */
+  {
+    path: '**',
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundPageModule)
+  }
 ];
 
 @NgModule({
